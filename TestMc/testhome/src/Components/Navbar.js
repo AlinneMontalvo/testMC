248f045a70b9d08d3a546fade3aa1865ebbfc487
Alinne <alinnemontalvocuna97@gmail.com>
2019-12-14 21:30:57 -0600

    navbar sin errores

---
+++ TestMc/testhome/src/Components/Navbar.js
@@ -0,0 +1,17 @@
+import React,{ Component } from 'react';
+
+class Navbar extends Component{
+    render (){
+        return(
+            // Inica navbar <!-- Image and text -->
+<nav className="navbar navbar-light bg-light">
+  <a className="navbar-brand" href="#">
+    <img src="../" width="30" height="30" class="d-inline-block align-top" alt=""/>
+    Bootstrap
+  </a>
+</nav>
+
+        );
+     }
+    }
+    export default Navbar;
